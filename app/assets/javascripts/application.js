35e68f9660869575ecbde81e9768ad84eb3bd145
sleepinglion <toughjjh@gmail.com>
2017-09-18 23:12:01 +0900

    수정함:        app/assets/javascripts/application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,18 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap/alert
+//= require bootstrap/modal
+
+$(document).ready(function(){
+$('#myModal').on('hidden.bs.modal', function () {
+    $(this).removeData('bs.modal');
+});
+
+$('.modal_link').click(function(event){
+  event.preventDefault();
+  $('#myModal').removeData("modal");
+  $('#myModal').load($(this).attr('href')+'?no_layout=true',function(){
+    $('#myModal').modal();
+  });
+});
+});
